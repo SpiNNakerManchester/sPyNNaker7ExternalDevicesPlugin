2d6d8b49dc72320b4ec5503a2df114a59c8ddfb8
Christian <brenninc@cs.man.ac.uk>
2017-04-24 16:31:27 +0100

    first requirements and setup files

---
+++ setup.py
@@ -0,0 +1,56 @@
+import os
+from setuptools import setup
+from collections import defaultdict
+
+exec(open("spynnaker7/pyNN/_version.py").read())
+
+if os.environ.get('READTHEDOCS', None) == 'True':
+    # scipy must be added in config.py as a mock
+    install_requires = ['sPyNNaker7 >= 3.0.0, < 4.0.0',
+                        'sPyNNakerExternalDevicesPlugin >= 3.0.0, < 4.0.0',
+                        'pyNN >= 0.7, < 0.8',
+                        'numpy', 'lxml', 'six', 'bitarray']
+else:
+    install_requires = ['sPyNNaker7 >= 3.0.0, < 4.0.0',
+                        'sPyNNakerExternalDevicesPlugin >= 3.0.0, < 4.0.0',
+                        'pyNN >= 0.7, < 0.8',
+                        'numpy', 'scipy', 'lxml', 'six', 'bitarray']
+
+# Build a list of all project modules, as well as supplementary files
+main_package = "spynnaker7_external_devices_plugin"
+data_extensions = {".aplx", ".xml", ".json", ".xsd"}
+config_extensions = {".cfg", ".template"}
+main_package_dir = os.path.join(os.path.dirname(__file__), main_package)
+start = len(main_package_dir)
+packages = []
+package_data = defaultdict(list)
+for dirname, dirnames, filenames in os.walk(main_package_dir):
+    if '__init__.py' in filenames:
+        package = "{}{}".format(
+            main_package, dirname[start:].replace(os.sep, '.'))
+        packages.append(package)
+    for filename in filenames:
+        _, ext = os.path.splitext(filename)
+        if ext in data_extensions:
+            package = "{}{}".format(
+                main_package, dirname[start:].replace(os.sep, '.'))
+            package_data[package].append("*{}".format(ext))
+            break
+        if ext in config_extensions:
+            package = "{}{}".format(
+                main_package, dirname[start:].replace(os.sep, '.'))
+            package_data[package].append(filename)
+
+setup(
+    name="sPyNNaker7ExternalDevicesPlugin", version="3.0.0",
+    description="Spinnaker External Devices Plugin extended for PyNN7",
+    url="https://github.com/SpiNNakerManchester/sPyNNaker7ExternalDevicesPlugin",
+    packages=packages,
+    package_data=package_data,
+    install_requires=install_requires
+)
+
+url = "https://github.com/SpiNNakerManchester/SpyNNaker",
+packages = packages,
+package_data = package_data,
+install_requires = ['sPyNNaker >= 3.0.0, < 4.0.0']
